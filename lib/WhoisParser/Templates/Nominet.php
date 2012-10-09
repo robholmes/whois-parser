5326c5a285ba77dc2cf8fc0747763ea16a1dd51f
Rob Holmes <email@robholmes.net>
2012-10-10 00:35:25 +0100

    UK Domain template testing

--- lib/WhoisParser/Templates/Nominet.php
+++ lib/WhoisParser/Templates/Nominet.php
@@ -45,8 +45,8 @@ class Template_Nominet extends AbstractTemplate
             1 => '/Registrant:[\r\n](.*?)[\n]{2}/is',
             2 => '/Name servers:[\r\n](.*?)[\n]{2}/is',
             3 => '/Relevant dates:[\r\n](.*?)[\n]{2}/is',
-            4 => '/Registrar:[\r\n](.*?)[\n]{2}$/is',
-            5 => '/Registration status:[\r\n](.*?)[\n]{2}$/is',
+            4 => '/Registrar:[\r\n](.*?)[\n]{2}/is',
+            5 => '/Registration status:[\r\n](.*?)[\n]{2}/is',
     );
 
     /**
